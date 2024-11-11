ce832252dfc33cedf9e36dfc158c3de3d5e2fe63
thegame615 <fitzgj3@rpi.edu>
2024-11-11 13:31:16 -0500

    something resembling functionality

--- Dijksra.hs
+++ Dijksra.hs
@@ -1,5 +1,6 @@
 import Data.Char (isSpace)
 import Data.List (foldl', minimumBy)
+import qualified Data.Map.Strict as Map
 import Data.Ord (comparing)
 
 -- Represent a node as a String
@@ -8,11 +9,8 @@ type Node = String
 -- Represent an edge as a triple of source, destination, and weight
 type Edge = (Node, Node, Int)
 
--- Represent the graph as a list of edges
-type Graph = [Edge]
-
--- Represent the shortest path information for each node
-type ShortestPath = [(Node, Int)]
+-- Represent the graph as a map of nodes to their adjacent nodes and weights
+type Graph = Map.Map Node [(Node, Int)]
 
 -- Parse a line of nodes file
 parseNode :: String -> Node
@@ -25,31 +23,33 @@ parseEdge line =
   in (src, dst, read w)
 
 -- Read nodes and edges from files
-readNodesAndEdges :: FilePath -> FilePath -> IO (Graph)
+readNodesAndEdges :: FilePath -> FilePath -> IO Graph
 readNodesAndEdges nodeFile edgeFile = do
   nodes <- lines <$> readFile nodeFile
   edges <- map parseEdge . lines <$> readFile edgeFile
-  return edges
+  return $ foldl' addEdge (Map.fromList [(n, []) | n <- nodes]) edges
+  where
+    addEdge :: Graph -> Edge -> Graph
+    addEdge g (s, d, w) = Map.update (Just . ((d, w) :)) s g
 
 -- Dijkstra's algorithm
-dijkstra :: Graph -> Node -> ShortestPath
+dijkstra :: Graph -> Node -> Map.Map Node Int
 dijkstra graph start =
   let
-    -- Initialize the shortest path information
-    initSP = [(n, if n == start then 0 else maxBound) | n <- nub (concatMap (\(s, d, _) -> [s, d]) graph)]
-    -- Helper function to find the next node to visit
-    nextNode sp = minimumBy (comparing snd) [(n, d) | (n, d) <- sp, d /= maxBound]
-    -- Dijkstra's algorithm implementation
+    initSP = Map.fromList [(n, if n == start then 0 else maxBound) | (n, _) <- Map.toList graph]
+    nextNode sp = minimumBy (comparing snd) [(n, d) | (n, d) <- Map.toList sp, d /= maxBound]
     go sp [] = sp
-    go sp ((s, d, w):es) =
+    go sp ((n, neighbors) : rest) =
       let
-        newDist = snd (head (filter ((== s) . fst) sp)) + w
-        sp' = [(if n == d then d else n, min (snd (head (filter ((== n) . fst) sp))) newDist) | (n, _) <- sp]
-      in go sp' es
-  in go initSP graph
+        newSP = foldl' (\acc (m, w) ->
+                        let dist = (maybe maxBound id (Map.lookup n acc)) + w
+                        in Map.insert m (min (maybe maxBound id (Map.lookup m acc)) dist) acc) sp neighbors
+      in go newSP rest
+  in go initSP (Map.toList graph)
 
 main :: IO ()
 main = do
   graph <- readNodesAndEdges "nodes.txt" "edges.txt"
   let shortestPaths = dijkstra graph "A"
+  print graph
   print shortestPaths
