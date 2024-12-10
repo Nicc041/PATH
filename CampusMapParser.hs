7b99faf5508056273b33a5b8fdaf6dd094c1580b
thegame615 <fitzgj3@rpi.edu>
2024-12-10 06:04:46 -0500

    working psoft

--- CampusMapParser.hs
+++ CampusMapParser.hs
@@ -1,10 +1,9 @@
-module CampusGraphParser
+module CampusMapParser
     ( Node(..)
     , Edge(..)
     , Graph
-    , readNodesAndEdges
+    , campusReadNodesAndEdges
     ) where
-
 import Text.Parsec
 import Text.Parsec.String
 import qualified Data.Map.Strict as Map
@@ -15,20 +14,15 @@ import Text.Parsec.Combinator (many1, manyTill, eof, skipMany1)
 import Text.Parsec.Prim (parse)
 
 -- Type aliases
-type Node = (String, Int, Int, Int)  -- (name, nodeId, x, y)
-type Edge = (Int, Int, Double)  -- (srcNodeId, dstNodeId, weight)
-type Graph = Map.Map Int [(Int, Double)]  -- Map from nodeId to list of (destinationNodeId, weight)
-
--- Skip spaces and any carriage return or newline
-skipWhitespace :: Parser ()
-skipWhitespace = skipMany1 (oneOf " \t\r\n")
+type Node = (String, String, Int, Int)  -- (name, nodeId, x, y)
+type Edge = (String, String, Int)  -- (srcNodeId, dstNodeId, weight)
+type Graph = Map.Map String [(String, Int)]  -- Map from node name to list of (destination node name, weight)
 
 -- Parse a node (name, id, x, y)
 nodeParser :: Parser Node
 nodeParser = do
   name <- option "" (manyTill anyChar (char ','))  -- The name can be empty if there is nothing before the comma
-  --name <- manyTill anyChar (char ',')  -- Parse node name up to the first comma
-  nodeId <- many1 digit  -- Parse node ID
+  nodeId <- many1 (alphaNum <|> char '_')  -- Parse node ID (allowing alphanumeric and underscore)
   _ <- char ','  -- Skip comma
   x <- many1 digit  -- Parse x coordinate
   _ <- char ','  -- Skip comma
@@ -36,7 +30,7 @@ nodeParser = do
   many1 (noneOf "\n")
   let finalName = if null name then nodeId else name
   
-  return (finalName, read nodeId, read x, read y)
+  return (finalName, nodeId, read x, read y)
 
 -- Parse a list of nodes (each line represents one node)
 nodesParser :: Parser [Node]
@@ -45,52 +39,53 @@ nodesParser = endBy1 nodeParser newline
 -- Parse an edge (srcNodeId, dstNodeId)
 edgeParser :: Parser Edge
 edgeParser = do
-  srcId <- many1 digit  -- Parse source node ID
+  srcId <- many1 (alphaNum <|> char '_')  -- Parse source node ID
   _ <- char ','  -- Skip comma
-  dstId <- many1 digit  -- Parse destination node ID
+  dstId <- many1 (alphaNum <|> char '_')  -- Parse destination node ID
   many1 (noneOf "\n")
-  return (read srcId, read dstId, 0)  -- Weight will be calculated later
+  return (srcId, dstId, 0)  -- Weight will be calculated later
 
 -- Parse a list of edges (each line represents one edge)
 edgesParser :: Parser [Edge]
 edgesParser = endBy1 edgeParser newline
 
 -- Calculate the distance between two nodes
-calculateDistance :: Node -> Node -> Double 
+calculateDistance :: Node -> Node -> Int
 calculateDistance (_, _, x1, y1) (_, _, x2, y2) =
-  sqrt (fromIntegral ((x2 - x1) ^ 2 + (y2 - y1) ^ 2))
+  round $ sqrt (fromIntegral ((x2 - x1) ^ 2 + (y2 - y1) ^ 2))
 
 -- Read nodes and edges from files and build the graph
-readNodesAndEdges :: FilePath -> FilePath -> IO Graph
-readNodesAndEdges nodeFile edgeFile = do
+campusReadNodesAndEdges :: FilePath -> FilePath -> IO Graph
+campusReadNodesAndEdges nodeFile edgeFile = do
   -- Parse nodes
   nodesResult <- parseFromFile nodesParser nodeFile
   nodes <- case nodesResult of
     Left err -> error $ "Error parsing nodes: " ++ show err
     Right parsedNodes -> return parsedNodes
+  
   -- Parse edges
   edgesResult <- parseFromFile edgesParser edgeFile
   edges <- case edgesResult of
     Left err -> error $ "Error parsing edges: " ++ show err
     Right parsedEdges -> return parsedEdges
-
+  
   -- Build graph by calculating edge weights based on distance
   let nodeMap = Map.fromList [(nodeId, n) | n@(name, nodeId, x, y) <- nodes]
-  let graphWithDistances = foldl' (addEdgeWithDistance nodeMap) (Map.fromList [(nodeId, []) | (_, nodeId, _, _) <- nodes]) edges
-
-  -- Print graph
-  putStrLn "\nGraph Structure:"
-  mapM_ (\(nodeId, connections) -> do
-    putStr $ "Node " ++ show nodeId ++ " connections: "
-    putStrLn $ show connections
-    ) (Map.toList graphWithDistances)
-
+  let nodeNameMap = Map.fromList [(nodeId, name) | (name, nodeId, _, _) <- nodes]
+  
+  let graphWithDistances = foldl' (addEdgeWithDistance nodeMap nodeNameMap) 
+                           (Map.fromList [(name, []) | (name, _, _, _) <- nodes]) 
+                           edges
+  
+  
   return graphWithDistances
   where
-    addEdgeWithDistance :: Map.Map Int Node -> Graph -> Edge -> Graph
-    addEdgeWithDistance nodeMap graph (src, dst, _) =
+    addEdgeWithDistance :: Map.Map String Node -> Map.Map String String -> Graph -> Edge -> Graph
+    addEdgeWithDistance nodeMap nodeNameMap graph (src, dst, _) =
       let Just srcNode = Map.lookup src nodeMap
           Just dstNode = Map.lookup dst nodeMap
+          Just srcName = Map.lookup src nodeNameMap
+          Just dstName = Map.lookup dst nodeNameMap
           dist = calculateDistance srcNode dstNode
-      in Map.update (Just . ((dst, dist) :)) src $
-         Map.update (Just . ((src, dist) :)) dst graph
+      in Map.update (Just . ((dstName, dist) :)) srcName $
+         Map.update (Just . ((srcName, dist) :)) dstName graph
--- Main.hs
+++ Main.hs
@@ -2,6 +2,7 @@ import GraphParser
 import Dijkstra (dijkstra)
 import BellmanFord (bellmanFord)
 import qualified Data.Map.Strict as Map
+import CampusMapParser(campusReadNodesAndEdges)
 
 -- Format distance for output
 formatDistance :: Maybe Int -> String
@@ -11,6 +12,53 @@ formatDistance (Just d) = show d
 -- Check for negative weights in the graph
 hasNegativeEdges :: Graph -> Bool
 hasNegativeEdges graph = any (\(_, weight) -> weight < 0) (concat (Map.elems graph))
+
+findCampusGraphPath :: IO()
+findCampusGraphPath = do
+  putStrLn "\nLoading graph..."
+  graph <- campusReadNodesAndEdges "RPI_map_data_Nodes.csv" "RPI_map_data_Edges.csv"
+  let nodes = Map.keys graph
+  
+  -- Display available nodes
+  putStrLn "\nAvailable nodes in the graph:"
+  mapM_ putStrLn nodes
+  
+  -- Prompt user for starting node
+  putStrLn "\nEnter the starting node:"
+  startNode <- getLine
+  if startNode `notElem` nodes
+    then putStrLn "Invalid starting node. Exiting..."
+    else do
+      -- Prompt user for algorithm choice
+      putStrLn "\nChoose algorithm:"
+      putStrLn "1. Dijkstra's Algorithm"
+      putStrLn "2. Bellman-Ford Algorithm"
+      choice <- getLine
+      
+      case choice of
+        "1" -> do
+          if hasNegativeEdges graph
+            then putStrLn "Error: Dijkstra's algorithm cannot be used with negative edge weights."
+            else do
+              putStrLn "\nRunning Dijkstra's Algorithm..."
+              let shortestPaths = dijkstra graph startNode
+              putStrLn "Shortest paths:"
+              mapM_ (\(node, dist) -> 
+                putStrLn $ node ++ ": " ++ formatDistance dist) 
+                (Map.toList shortestPaths)
+        
+        "2" -> do
+          putStrLn "\nRunning Bellman-Ford Algorithm..."
+          case bellmanFord graph startNode of
+            Left errorMsg -> putStrLn $ "Error: " ++ errorMsg
+            Right shortestPaths -> do
+              putStrLn "Shortest paths:"
+              mapM_ (\(node, dist) -> 
+                putStrLn $ node ++ ": " ++ formatDistance dist) 
+                (Map.toList shortestPaths)
+        
+        _ -> putStrLn "Invalid choice. Please select 1 or 2."
+  
 
 main :: IO ()
 main = do
